314bd2daac8866aa5b56c875d6e82c68ae38c20e
DrEPL <dolnickenzanza@gmail.com>
2025-05-06 01:30:34 +0200

    conflit ok

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'sn.itcreative.faydah',
   appName: 'faydah',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    StatusBar: {
+      style: "light",
+      backgroundColor: "#ffffff",
+      overlaysWebView: false,
+    },
+  },
 };
 
 export default config;
